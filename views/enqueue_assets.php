e145c1060bd6ea7bb4ee75e0bd9e7de39f595ae9
Shay <chenille.pottle@yahoo.co.uk>
2024-06-15 14:23:19 +0100

    Fix enqueue refs

--- views/enqueue_assets.php
+++ views/enqueue_assets.php
@@ -12,30 +12,30 @@ function skb_enqueues() {
     if( isset($current_page) ){
         
         if( in_array($current_page, $pages_needing_jquery) ){
-            //wp_enqueue_script('jquery', VIEWS_DIR . 'assets/frontend/js/jquery.js', ['jquery'], '1.0', true);
+            //wp_enqueue_script('jquery', plugins_url('assets/frontend/js/jquery.js', ['jquery'], '1.0', true);
         } // end $pages_needing_jquery check
         
         if( in_array($current_page, $pages_needing_jkanban) ){
-            wp_enqueue_style('kanban', VIEWS_DIR . 'assets/backend/kanban/jkanban.css', [], '1.0', 'all');
-            wp_enqueue_script('kanban-js', VIEWS_DIR . 'assets/backend/kanban/jkanban.js', [], '1.0', false);
+            wp_enqueue_style('kanban', plugins_url('assets/kanban/jkanban.css', __FILE__ ), '1.0', 'all');
+            wp_enqueue_script('kanban-js', plugins_url('assets/kanban/jkanban.js', __FILE__ ), '1.0', false);
         } // end $pages_needing_jkanban check
         
         /* STYLES */
-        wp_enqueue_style('nucleo-icons', VIEWS_DIR . 'assets/backend/css/nucleo-icons.css', [], '1.0', 'all');
-        wp_enqueue_style('nucleo-svg', VIEWS_DIR . 'assets/backend/css/nucleo-svg.css', [], '1.0', 'all');
-        wp_enqueue_style('material-dashboard', VIEWS_DIR . 'assets/backend/css/material-dashboard.css?v=3.1.0', [], '1.0', 'all');
+        wp_enqueue_style('nucleo-icons', plugins_url('assets/css/nucleo-icons.css', __FILE__ ), '1.0', 'all');
+        wp_enqueue_style('nucleo-svg', plugins_url('assets/css/nucleo-svg.css', __FILE__ ), '1.0', 'all');
+        wp_enqueue_style('material-dashboard', plugins_url('assets/css/material-dashboard.css?v=3.1.0', __FILE__ ), '1.0', 'all');
         
         /* SCRIPTS */
-        wp_enqueue_script('popper', VIEWS_DIR . 'assets/backend/js/core/popper.min.js', [], '1.0', true);
-        wp_enqueue_script('bootstrap', VIEWS_DIR . 'assets/backend/js/core/bootstrap.min.js', [], '1.0', true);
-        wp_enqueue_script('perfect-scrollbar', VIEWS_DIR . 'assets/backend/js/plugins/perfect-scrollbar.min.js', [], '1.0', true);
-        wp_enqueue_script('smooth-scrollbar', VIEWS_DIR . 'assets/backend/js/plugins/smooth-scrollbar.min.js', [], '1.0', true);
+        wp_enqueue_script('popper', plugins_url('assets/js/core/popper.min.js', __FILE__ ), '1.0', true);
+        wp_enqueue_script('bootstrap', plugins_url('assets/js/core/bootstrap.min.js', __FILE__ ), '1.0', true);
+        wp_enqueue_script('perfect-scrollbar', plugins_url('assets/js/plugins/perfect-scrollbar.min.js', __FILE__ ), '1.0', true);
+        wp_enqueue_script('smooth-scrollbar', plugins_url('assets/js/plugins/smooth-scrollbar.min.js', __FILE__ ), '1.0', true);
         
         // Control Center for Material Dashboard: parallax effects, scripts for the example pages etc
-        wp_enqueue_script('material-dashboard', VIEWS_DIR . 'assets/backend/js/material-dashboard.min.js?v=3.1.0', [], '1.0', true);
+        wp_enqueue_script('material-dashboard', plugins_url('assets/js/material-dashboard.min.js?v=3.1.0', __FILE__ ), '1.0', true);
         // Also houses the label animation for videos, don't delete
       
-        wp_enqueue_script('pws_global', VIEWS_DIR . 'assets/global/js/pws_global.js', [], '1.0', true);
+        wp_enqueue_script('pws_global', plugins_url('assets/global/js/pws_global.js', __FILE__ ), '1.0', true);
         
         
     } // end isset($current_page)
